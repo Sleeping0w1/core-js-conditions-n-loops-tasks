3704746848c6b3553a07539e19813b8e90f3b8dd
Sleeping0w1 <1033351@gmail.com>
2024-12-15 22:18:50 +0300

    feat: solution for sortByAsc

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -399,8 +399,18 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const sortedArr = arr;
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < arr.length - i - 1; j += 1) {
+      if (sortedArr[j] > sortedArr[j + 1]) {
+        const res = sortedArr[j];
+        sortedArr[j] = sortedArr[j + 1];
+        sortedArr[j + 1] = res;
+      }
+    }
+  }
+  return sortedArr;
 }
 
 /**
